33c226541a14f657724cc8cbf87141efc600080e
Ad-h0c <reddycsivaprakash5@gmail.com>
2022-08-29 15:38:37 +0530

    updated hardhat config file

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,20 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("@nomicfoundation/hardhat-chai-matchers");
+require("dotenv").config();
 /** @type import('hardhat/config').HardhatUserConfig */
+const { GOERLI_PRIVATE_KEY, ALCHEMY_API_KEY, YOUR_ETHERSCAN_API_KEY } =
+  process.env;
 module.exports = {
   solidity: "0.8.9",
+  networks: {
+    goerli: {
+      url: `https://eth-goerli.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
+      accounts: [GOERLI_PRIVATE_KEY],
+    },
+  },
+  etherscan: {
+    // Your API key for Etherscan
+    // Obtain one at https://etherscan.io/
+    apiKey: YOUR_ETHERSCAN_API_KEY,
+  },
 };
